5266b758e64e49de480ca29609b3c9d7a900a6f9
LunaParajuli <parajuliluna8@gmail.com>
2024-09-20 11:53:06 +0545

    js10

--- JavaScript/class8_imageslider.js
+++ JavaScript/class8_imageslider.js
@@ -1,14 +1,41 @@
-array_image = ['apple.jpg', 'keyboard.avif', 'group.avif', 'laptop1.jpg', 'laptop2.jpg']
+array_image = ['apple.jpg', 'keyboard.avif', 'group.avif', 'laptop1.jpg']
 let array_length = array_image.length
 let i = 0
 
 const sliderFunction = () => {
-  if (i < 3) {
+  if (i >= array_length) {
     i = 0
   }
+
   document.querySelector('#slider_img').src = `image/${array_image[i]}`
-  // console.log(`image/${array_image[i]}`)
-  i++
+  i++;
 }
 
-setInterval(sliderFunction, 3000)
+let set = setInterval(sliderFunction, 3000);
+
+//Event listener for the right button (next image)
+document.querySelector('.image-slider .right').addEventListener('click', () => {
+  if (i >= array_length) {
+    i = 0;
+  }
+  document.querySelector('#slider_img').src = `image/${array_image[i]}`;
+  i++;
+});
+
+document.querySelector('.image-slider .left').addEventListener('click', () => {
+  if (i < 0) {
+    i = array_length - 1
+  }
+
+  document.querySelector('img').src = `image/${array_image[i]}`
+  i--
+})
+
+
+document.querySelector('.image-slider').addEventListener('mouseover', () => {
+  clearInterval(set)
+})
+
+document.querySelector('.image-slider').addEventListener('mouseout', () => {
+  set = setInterval(sliderFunction, 3000);
+})
